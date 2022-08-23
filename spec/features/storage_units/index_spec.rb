340eee13abb1411e5a4411c9dad5bbf1ac68d4ec
Andrew Mullins <mullinsand15@gmail.com>
2022-08-23 17:05:43 -0600

    update testing folders for additional testing

--- spec/features/storage_units/index_spec.rb
+++ spec/features/storage_units/index_spec.rb
@@ -1,13 +1,14 @@
-# User Story 5, Parent Children Index 
+# User Story 1, Parent Index 
 
+# For each parent table
 # As a visitor
-# When I visit '/parents/:parent_id/child_table_name'
-# Then I see each Child that is associated with that Parent with each Child's attributes:
+# When I visit '/parents'
+# Then I see the name of each parent record in the system
 
 require 'rails_helper'
 
-RSpec.describe 'Storage Unit chemicals index' do
-  it 'shows all of the names of the chemicals for each storage unit' do
+RSpec.describe 'Storage Unit index' do
+  it 'shows all of the names of each storage unit' do
     lab1 = StorageUnit.create!(name: 'lab1', size: 3.0, fireproof: true)
     ethanol = lab1.chemicals.create!(name: 'ethanol', amount: 3.00, flammable: true)
     methanol = lab1.chemicals.create!(name: 'methanol', amount: 6.00, flammable: true)
