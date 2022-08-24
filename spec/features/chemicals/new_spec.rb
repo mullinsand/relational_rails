0e28af5bd7b0d6a153e486de207a8881ad329552
Andrew Mullins <mullinsand15@gmail.com>
2022-08-24 15:17:31 -0600

    add test fornew chemical link in storage unit chemicals index

--- spec/features/chemicals/new_spec.rb
+++ spec/features/chemicals/new_spec.rb
@@ -1,7 +1,21 @@
-
-
 require 'rails_helper'
 
-RSpec.describe 'Chemical new' do
+RSpec.describe 'chemical new' do
+  describe 'form for creating new chemical record' do
+    describe 'as a user' do
+      it 'has a form to submit chemical attributes' do
+        visit '/chemicals/new'
+
+        fill_in('name', with: 'isopropanol')
+        fill_in('amount', with: 300.00)
+        choose('flammable', with: true)
+        click_button('Create Chemical')
+        
+        new_chemical_id = Chemical.last.id
+        expect(current_path).to eq('/chemicals')
+        expect(page).to have_content('isopropanol')
+      end
+    end
+  end
 
 end
