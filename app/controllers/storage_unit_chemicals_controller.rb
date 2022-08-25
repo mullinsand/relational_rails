e270c11c8c6061557e78e6b0012042f373481ee6
Andrew Mullins <mullinsand15@gmail.com>
2022-08-24 21:17:47 -0600

    test and feature threshold search

--- app/controllers/storage_unit_chemicals_controller.rb
+++ app/controllers/storage_unit_chemicals_controller.rb
@@ -3,6 +3,8 @@ class StorageUnitChemicalsController < ApplicationController
     @storage_unit = StorageUnit.find(params[:id])
     @chemicals = if params[:sort] == 'name'
       @storage_unit.chemicals.order(:name)
+    elsif params[:threshold_search]
+      @storage_unit.chemicals.select { |chemical| chemical.amount > params[:threshold_search].to_f}
     else
       @storage_unit.chemicals
     end
